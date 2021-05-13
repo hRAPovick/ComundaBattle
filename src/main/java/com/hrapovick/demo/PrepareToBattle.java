12f048d2f860e4fae298a38cf19d516b165be6aa
hRAPovick <qsub.prod@gmail.com>
2021-05-13 10:19:24 +0300

    simple battle with check on right number warriors

---
+++ src/main/java/com/hrapovick/demo/PrepareToBattle.java
@@ -0,0 +1,36 @@
+package com.hrapovick.demo;
+
+import org.camunda.bpm.engine.delegate.BpmnError;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PrepareToBattle implements JavaDelegate {
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) {
+
+        int warriors = (int) delegateExecution.getVariable("warriors");
+        int enemyWarriors = (int) (Math.random() * 100);
+        String battleStatus = "Undefined";
+        boolean isWin = false;
+
+        // Error boundary event
+        if (warriors < 1 || warriors > 100) {
+            throw new BpmnError("warriorsError");
+        }
+
+        if ((warriors - enemyWarriors) > 0) {
+            isWin = true;
+            battleStatus = "Victory!";
+        } else {
+            battleStatus = "Fail! :(";
+        }
+
+        delegateExecution.setVariable("enemyWarriors", enemyWarriors);
+        delegateExecution.setVariable("battleStatus", battleStatus);
+        delegateExecution.setVariable("isWin", isWin);
+
+    }
+}
