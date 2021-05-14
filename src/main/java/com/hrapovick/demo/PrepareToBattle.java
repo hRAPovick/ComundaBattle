1045d7ddb81e3e53db2abdc6bd0269fdc2ab8038
hRAPovick <qsub.prod@gmail.com>
2021-05-14 12:58:53 +0300

    warriors.class create from method, which use controller

--- src/main/java/com/hrapovick/demo/PrepareToBattle.java
+++ src/main/java/com/hrapovick/demo/PrepareToBattle.java
@@ -1,14 +1,19 @@
 package com.hrapovick.demo;
 
+import com.hrapovick.demo.domain.Warrior;
 import org.camunda.bpm.engine.delegate.BpmnError;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
-import org.springframework.stereotype.Component;
+import org.camunda.connect.Connectors;
+import org.camunda.connect.httpclient.HttpConnector;
+import org.camunda.connect.httpclient.HttpRequest;
+import org.camunda.connect.httpclient.HttpResponse;
+import org.camunda.spin.Spin;
+import org.camunda.spin.json.SpinJsonNode;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @Component
 public class PrepareToBattle implements JavaDelegate {
@@ -16,11 +21,14 @@ public class PrepareToBattle implements JavaDelegate {
     @Value("${maxWarriors}")
     private int maxWarriors;
 
+    @Value("${url}")
+    private String url;
+
     @Override
     public void execute(DelegateExecution delegateExecution) {
 
         int warriors = (int) delegateExecution.getVariable("warriors");
-        int enemyWarriors = (int) (Math.random() * 100);
+        int enemyWarriors = new Random().nextInt(100);
 
         maxWarriors = maxWarriors == 0 ? 100 : maxWarriors;
 
@@ -29,10 +37,40 @@ public class PrepareToBattle implements JavaDelegate {
             throw new BpmnError("warriorsError");
         }
 
-        List<Boolean> army = new ArrayList<>(Collections.nCopies(warriors, true));
+        List<Warrior> army = new ArrayList<>();
+
+        for(int i = 0; i <= warriors; i++) {
+            army.add(create());
+        }
+
         System.out.println(String.format("Prepare to battle! Enemy army = %s vs. our army %s", enemyWarriors, warriors));
         delegateExecution.setVariable("army", army);
         delegateExecution.setVariable("enemyWarriors", enemyWarriors);
 
+    }
+
+    private Warrior create(){
+
+        Warrior warrior = new Warrior();
+
+        HttpConnector httpConnector = Connectors.getConnector(HttpConnector.ID);
+        HttpRequest httpRequest = httpConnector.createRequest()
+                .get()
+                .url(url);
+        Map<String, String> headers = new HashMap<>();
+        httpRequest.setRequestParameter("headers", headers);
+
+        headers.put("Content-type", "application/json");
+
+        HttpResponse response = httpRequest.execute();
+        if (response.getStatusCode() == 200) {
+            SpinJsonNode node = Spin.JSON(response.getResponse());
+            warrior.setTitle(node.prop("name").stringValue());
+            warrior.setName(node.prop("title").stringValue());
+            warrior.setHp((Integer) node.prop("hp").numberValue());
+            warrior.setIsAlive(true);
+        }
+        response.close();
+        return warrior;
     }
 }
---
+++ src/main/java/com/hrapovick/demo/domain/Warrior.java
@@ -0,0 +1,22 @@
+package com.hrapovick.demo.domain;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Warrior implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @JsonAlias("name.findName")
+    private String name;
+    @JsonAlias("name.title")
+    private String title;
+    private Boolean isAlive;
+    @JsonAlias("random.number")
+    private Integer hp;
+}
